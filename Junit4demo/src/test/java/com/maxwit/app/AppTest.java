0ff8c4a725880ba87f6b1cd5324cddefb2e6a2a3
Hande Zhou <hande.zhou@foxmail.com>
2019-12-18 16:06:27 +0800

    Junit4 demo
    Resolve #1
    
    Signed-off-by: Hande Zhou <hande.zhou@foxmail.com>

---
+++ Junit4demo/src/test/java/com/maxwit/app/AppTest.java
@@ -0,0 +1,23 @@
+package com.maxwit.app;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals; //static 作用是方便使用类名直接调用成员方法
+
+//import org.junit.Assert;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+
+    @Test
+    public void AssertEquals() {
+        App app = new App();
+        assertEquals(2, app.add(1, 4));
+    }
+}
